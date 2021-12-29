d220d60c417d4cfebde42ae02bac41a24f0f930b
Radomír Polách <rp@t4d.cz>
2021-12-29 01:13:46 +0100

    feature: improved brigthness curve

--- menu.sh
+++ menu.sh
@@ -4,7 +4,8 @@ dialog --checklist          'Choose the desired patches' 0 0 0 \
     fn-swap                 'Swap Fn and Ctrl keys' off \
     fix-ec-debug            'Allow hot-patching of EC.' on \
     default-lcd-brightness  'Set default LCD backlight brightness behaviour' off \
-    lcd-brightness          'Allow lowering LCD backlight brightness to 1%' on \
+    lcd-brightness          'Allow lowering LCD backlight brightness to 1% (1%,2%,4%,8%,16%,32%,44%,48%,55%,60%,65%,70%,78%,84%,94%,99%)' on \
+    improved-lcd-brightness 'Allow lowering LCD backlight brightness to 1% (1%,2%,4%,8%,14%,21%,30%,44%,48%,55%,61%,66%,73%,80%,92%,99%)' off \
     lcd-backlight-925hz     'LCD backlight to 925Hz' off \
     true-battery-reading    'Fix battery reading above 70%' on \
     battery-current         'Fix battery current measurement' on \
@@ -33,7 +34,12 @@ for p in $(cat selected); do
     fi
     if [[ "$p" =~ .*fan-pwm-table$ ]]; then
         set +x
-        echo -e "PWM TABLE:\n  in  out  pwm"; cat patches/$p.rapatch | cut -d" " -f2 | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g" | awk '{ print sprintf("%4d", strtonum("0x" $1)), sprintf("%4d", strtonum("0x" $2)),  sprintf("%4d", strtonum("0x" $3)) }'
+        echo -e "PWM FAN TABLE:\n  in  out  pwm"; cat patches/$p.rapatch | cut -d" " -f2 | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g" | awk '{ print sprintf("%4d", strtonum("0x" $1)), sprintf("%4d", strtonum("0x" $2)),  sprintf("%4d", strtonum("0x" $3)) }'
+        set -x
+    fi
+    if [[ "$p" =~ .*lcd-brightness$ ]]; then
+        set +x
+        echo -e "PWM BRIGHTNESS TABLE:\n  acc   bat"; cat patches/$p.rapatch | cut -d" " -f2 | sed "s/.\{4\}/&\n/g" | head -n -1 | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g" | awk '{ print sprintf("%4d%", strtonum("0x" $1)), sprintf("%4d%", strtonum("0x" $2)) }'
         set -x
     fi
     r2 -w -q -P "$fn" ec.bin || true    # see https://github.com/radare/radare2/issues/15002
